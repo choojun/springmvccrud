b5e2ba6e41ddad4b6345da12119345ee7a7adc50
ChooJun <choojun@users.noreply.github.com>
2022-03-18 15:43:59 +0800

    Update User.java
    with field validation

--- src/main/java/edu/wou/choojun/entity/User.java
+++ src/main/java/edu/wou/choojun/entity/User.java
@@ -13,10 +13,12 @@ public class User
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	// @NotBlank(message = "Name is mandatory")
+	
+	@NotNull
+	@Size(min = 3, max = 10)
 	private String name;
 
-	// @NotBlank(message = "Email is mandatory")
+	@Email
 	private String email;
 
 	public User()
