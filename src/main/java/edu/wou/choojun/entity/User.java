e2c7a41c8b567e42d5290f59941dbd28fe786fd2
ChooJun <choojun@users.noreply.github.com>
2022-03-18 15:45:58 +0800

    Update User.java
    with field validation

--- src/main/java/edu/wou/choojun/entity/User.java
+++ src/main/java/edu/wou/choojun/entity/User.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class User
